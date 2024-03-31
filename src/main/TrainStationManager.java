737ee51a64822383e5520e956358902d1a8bfb0b
keisanabria <keishlyany.sanabria@upr.edu>
2024-03-31 14:08:34 -0400

    Progress on TrainsStationManager(String station_file) <doesn't compile>

--- src/main/TrainStationManager.java
+++ src/main/TrainStationManager.java
@@ -4,10 +4,20 @@ import interfaces.List;
 import interfaces.Map;
 import interfaces.Stack;
 
+/* My imports below */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import data_structures.HashTableSC;
+import data_structures.SimpleHashFunction;
+
 public class TrainStationManager {
 	
-	// Initiate Map<String, List<Station>> stations
-		// private Map<String, List<Station>> stations = new Map<String, List<Station>>();
+	private Map<String, List<Station>> stations = new HashTableSC<>(1, new SimpleHashFunction<>());
+	private String[] parts;
+	private String src_city;
+	private String station;
 	// Initiate Map<String, Station> shortRoutes
 	
 	/* Reads the file given by station_file and populates the stations map */
@@ -15,6 +25,27 @@ public class TrainStationManager {
 		/* Calls findShortestDistance() for "Westside" so that the shortest
 		routes will be in reference to Westside. */
 		
+		try (BufferedReader reader = new BufferedReader(new FileReader(station_file))) {
+            String line;
+            int lineNum = 1;
+            while ((line = reader.readLine()) != null) {
+                if(lineNum != 1) { // If it is the first line, it skips it
+                	// If it contains key (use containsKey method), just add the dest_city to the List<Station>
+                	// Else, add the key
+                	
+                	parts = line.split(",", 2);
+                	src_city = parts[0];
+                	station = parts[1];
+                	if(stations.containsKey( /* dest_city */ )) {
+                		
+                	}
+                	stations.put( , );
+                }
+                lineNum++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 	
 	/* Calculates the shortest route from “Westside” to every other station following 
