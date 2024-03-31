286b91b705538c36f6804d47640f7ab76cd49b96
keisanabria <keishlyany.sanabria@upr.edu>
2024-03-30 21:00:34 -0400

    Added comments

--- src/main/TrainStationManager.java
+++ src/main/TrainStationManager.java
@@ -5,23 +5,35 @@ import interfaces.Map;
 import interfaces.Stack;
 
 public class TrainStationManager {
-
+	
+	// Initiate Map<String, List<Station>> stations
+		// private Map<String, List<Station>> stations = new Map<String, List<Station>>();
+	// Initiate Map<String, Station> shortRoutes
+	
+	/* Reads the file given by station_file and populates the stations map */
 	public TrainStationManager(String station_file) {
-		
+		/* Calls findShortestDistance() for "Westside" so that the shortest
+		routes will be in reference to Westside. */
 		
 	}
 	
+	/* Calculates the shortest route from “Westside” to every other station following 
+	 * the logic given in the “Shortcuts to Victory” section. It populates
+	 * the shortest route map. */
 	private void findShortestDistance() {
 				
 	}
 
+	/* Receives a Stack that needs to remain sorted and the station we want to add. */
 	public void sortStack(Station station, Stack<Station> stackToSort) {
-		
+		// Create alg to keep the stack sorted (Reference: "How do we keep the stack sorted?" section)
 	}
 	
+	/*  Returns a Map where the key is the station name, and the value is 
+	 * the time it takes to reach that station. */
 	public Map<String, Double> getTravelTimes() {
-		// 5 minutes per kilometer
-		// 15 min per station
+		// 2.5 minutes per kilometer (use the shortest distance for this)
+		// 15 min per station (between Westside and the destination)
 		
 	}
 
